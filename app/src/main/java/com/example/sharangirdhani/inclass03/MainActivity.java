760b02d352331bb9a2407f636369023d71c8be90
salman mujtaba <salmanmujtaba1793@gmail.com>
2017-09-11 17:53:22 -0400

    Databinding

--- app/src/main/java/com/example/sharangirdhani/inclass03/MainActivity.java
+++ app/src/main/java/com/example/sharangirdhani/inclass03/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.sharangirdhani.inclass03;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.example.sharangirdhani.inclass03.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.d("demo", "salman");
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        binding.textView2.setText("Salman");
     }
 }
