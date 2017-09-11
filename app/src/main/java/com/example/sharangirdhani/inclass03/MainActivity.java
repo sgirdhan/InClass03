8fa212b63e69bc02d754ff65e8f947109da4421d
Sharan Girdhani <sgirdhan@uncc.edu>
2017-09-11 19:23:06 -0400

    Initial Design and images

--- app/src/main/java/com/example/sharangirdhani/inclass03/MainActivity.java
+++ app/src/main/java/com/example/sharangirdhani/inclass03/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.sharangirdhani.inclass03;
 
 import android.databinding.DataBindingUtil;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.sharangirdhani.inclass03.databinding.ActivityMainBinding;
 
